4e788e5689c11fb1395b199c1385bcc3d1ace639
Nail Khafizov <nailkhaf@pm.me>
2021-04-07 10:18:43 +0300

    convert tabs to spaces

--- packages/relay/src/forwarder.js
+++ packages/relay/src/forwarder.js
@@ -6,29 +6,29 @@ const Wallet = require('ethereumjs-wallet').default
 
 class Forwarder {
 
-	constructor() {
-		this.forwarder = undefined
-	}
+    constructor() {
+        this.forwarder = undefined
+    }
 
-	async createForwarder(provider) {
-		const forwarderAddress = await TrustedForwarder.getAddress(provider)
-		this.forwarder = new TrustedForwarder(forwarderAddress, provider)
-	}
+    async createForwarder(provider) {
+        const forwarderAddress = await TrustedForwarder.getAddress(provider)
+        this.forwarder = new TrustedForwarder(forwarderAddress, provider)
+    }
 
-	async relay(request) {
+    async relay(request) {
 
-		if (!this.forwarder) {
-			throw new Error("Forwarder is not created")
-		}
+        if (!this.forwarder) {
+            throw new Error("Forwarder is not created")
+        }
 
-		const { to, from, value, gas, nonce, data, signature } = request
+        const { to, from, value, gas, nonce, data, signature } = request
 
-		const args = [{ to, from, value, gas, nonce, data }, signature]
+        const args = [{ to, from, value, gas, nonce, data }, signature]
 
-		await this.forwarder.contract.verify(...args)
+        await this.forwarder.contract.verify(...args)
 
-		return await this.forwarder.contract.execute(...args, {gasLimit: 10000000}) // estimate gas limit
-	}
+        return await this.forwarder.contract.execute(...args, {gasLimit: 10000000}) // estimate gas limit
+    }
 }
 
 module.exports.Forwarder = Forwarder
--- packages/relay/src/provider.js
+++ packages/relay/src/provider.js
@@ -8,31 +8,31 @@ const NETWORK = process.env.NETWORK
 
 class DefenderProvider {
 
-	constructor() {
-		this.provider = undefined
-	}
+    constructor() {
+        this.provider = undefined
+    }
 
-	getProvider() {
-		if (!this.provider) {
-			throw new Error("Provider is not created")
-		}
-		return this.provider
-	}
+    getProvider() {
+        if (!this.provider) {
+            throw new Error("Provider is not created")
+        }
+        return this.provider
+    }
 
-	getRelayer() {
-		if (!this.relayer) {
-			throw new Error("Relayer is not created")
-		}
-		return this.relayer
-	}
+    getRelayer() {
+        if (!this.relayer) {
+            throw new Error("Relayer is not created")
+        }
+        return this.relayer
+    }
 
-	createProvider(credentials) {
-	    const provider = new DefenderRelayProvider(credentials)
-	    this.provider = new DefenderRelaySigner(credentials, provider, { speed: DEFENDER_SPEED })
-	    this.provider.getNetwork = () => ({name: NETWORK})
-		const relayer = new Relayer(credentials)
-		this.relayer = relayer
-	}
+    createProvider(credentials) {
+        const provider = new DefenderRelayProvider(credentials)
+        this.provider = new DefenderRelaySigner(credentials, provider, { speed: DEFENDER_SPEED })
+        this.provider.getNetwork = () => ({name: NETWORK})
+        const relayer = new Relayer(credentials)
+        this.relayer = relayer
+    }
 }
 
 module.exports.DefenderProvider = DefenderProvider
